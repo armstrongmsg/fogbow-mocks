b589ca066da8c28b7456376ee2a9eac33e2a5685
Armstrong Goes <armstrongmsg@gmail.com>
2021-04-05 16:27:09 -0300

    Fixed bug in volume record generation and small refactoring

--- accs/accs.py
+++ accs/accs.py
@@ -42,67 +42,79 @@ class ACCSMock:
         self.config = configparser.ConfigParser()
         self.config.read("accs.ini")
 
-    def get_user_usage(self, user_id, requester_id, provider_id, resource, start_date, end_date):
+    def _get_compute_usage(self, user_id, requester_id, provider_id, start_date, end_date):
         recordId = int(self.config["compute"]["recordId"])
         orderId = self.config["compute"]["orderId"]
+        specId = int(self.config["compute"]["specId"])
+        vCpu = int(self.config["compute"]["vCpu"])
+        ram = int(self.config["compute"]["ram"])
+        requester = self.config["compute"]["requester"]
+        startTime = self.config["compute"]["startTime"]
+        startDate = self.config["compute"]["startDate"]
+        endDate = self.config["compute"]["endDate"]
+        endTime = self.config["compute"]["endTime"]
+        duration = int(self.config["compute"]["duration"])
+        state = self.config["compute"]["state"]
+
+        r = {
+                "id": recordId, 
+                "orderId": orderId,
+                "resourceType": "compute",
+                "spec": {
+                            "id": specId, 
+                            "vCpu":vCpu, 
+                            "ram":ram
+                        }, 
+                "requester": requester,
+                "startTime": startTime,
+                "startDate": startDate,
+                "endDate": endDate,
+                "endTime": endTime,
+                "duration": duration,
+                "state": state
+            }
+        
+        return r
+
+    def _get_volume_usage(self, user_id, requester_id, provider_id, start_date, end_date):
+        recordId = int(self.config["volume"]["recordId"])
+        orderId = self.config["volume"]["orderId"]
+        specId = int(self.config["volume"]["specId"])
+        size = int(self.config["volume"]["size"])
+        requester = self.config["volume"]["requester"]
+        startTime = self.config["volume"]["startTime"]
+        startDate = self.config["volume"]["startDate"]
+        endDate = self.config["volume"]["endDate"]
+        endTime = self.config["volume"]["endTime"]
+        duration = int(self.config["volume"]["duration"])
+        state = self.config["volume"]["state"]
+
+        r = {
+                "id": recordId, 
+                "orderId": orderId,
+                "resourceType": "volume",
+                "spec": {
+                            "id": specId, 
+                            "size":size
+                        },
+                "requester": requester,
+                "startTime": startTime,
+                "startDate": startDate,
+                "endDate": endDate,
+                "endTime": endTime,
+                "duration": duration,
+                "state": state
+            }
+
+        return r
+
+    def get_user_usage(self, user_id, requester_id, provider_id, resource, start_date, end_date):
         r = None
 
         if resource == "compute":
-            specId = int(self.config["compute"]["specId"])
-            vCpu = int(self.config["compute"]["vCpu"])
-            ram = int(self.config["compute"]["ram"])
-            requester = self.config["compute"]["requester"]
-            startTime = self.config["compute"]["startTime"]
-            startDate = self.config["compute"]["startDate"]
-            endDate = self.config["compute"]["endDate"]
-            endTime = self.config["compute"]["endTime"]
-            duration = int(self.config["compute"]["duration"])
-            state = self.config["compute"]["state"]
-
-            r = {
-                    "id": recordId, 
-                    "orderId": orderId,
-                    "resourceType": "compute",
-                    "spec": {
-                                "id": specId, 
-                                "vCpu":vCpu, 
-                                "ram":ram
-                            }, 
-                    "requester": requester,
-                    "startTime": startTime,
-                    "startDate": startDate,
-                    "endDate": endDate,
-                    "endTime": endTime,
-                    "duration": duration,
-                    "state": state
-                }
+            r = self._get_compute_usage(user_id, requester_id, provider_id, start_date, end_date)
         elif resource == "volume":
-            specId = int(self.config["volume"]["specId"])
-            size = int(self.config["volume"]["size"])
-            requester = self.config["volume"]["requester"]
-            startTime = self.config["volume"]["startTime"]
-            startDate = self.config["volume"]["startDate"]
-            endDate = self.config["volume"]["endDate"]
-            endTime = self.config["volume"]["endTime"]
-            duration = int(self.config["volume"]["duration"])
-            state = self.config["volume"]["state"]
-
-            r = {
-                    "id": recordId, 
-                    "orderId": orderId,
-                    "resourceType": "volume",
-                    "spec": {
-                                "id": specId, 
-                                "size":size
-                            },
-                    "requester": requester,
-                    "startTime": startTime,
-                    "startDate": startDate,
-                    "endDate": endDate,
-                    "endTime": endTime,
-                    "duration": duration,
-                    "state": state
-                }
+            r = self._get_volume_usage(user_id, requester_id, provider_id, start_date, end_date)
 
         return jsonpickle.encode([r])
 
@@ -112,17 +124,14 @@ class ACCSMock:
         key = file.read()
         file.close()
 
+        key = key.replace("\n", "")
         c = {"publicKey": key}
 
         return jsonpickle.encode(c)
 
 app = Flask(__name__)
-
-@app.route('/hello')
-def hello():
-    return 'dfjadofjadifjsdfd',200
-
 m = ACCSMock()
+
 @app.route('/accs/usage/<user_id>/<requester_id>/<provider_id>/<resource>/<start_date>/<end_date>', methods = ['GET'])
 def get_user_usage(user_id, requester_id, provider_id, resource, start_date, end_date):
     return m.get_user_usage(user_id, requester_id, provider_id, resource, start_date, end_date),200
@@ -130,5 +139,5 @@ def get_user_usage(user_id, requester_id, provider_id, resource, start_date, end
 @app.route('/accs/publicKey', methods = ['GET'])
 def get_public_key():
     return m.get_public_key(),200
-app.run(host=sys.argv[1], port=sys.argv[2], debug=True)
 
+app.run(host=sys.argv[1], port=sys.argv[2], debug=True)
