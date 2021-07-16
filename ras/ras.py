d6c611403c77ddeedebcc1619fcc9557bd068261
Armstrong Goes <armstrongmsg@gmail.com>
2021-07-16 16:05:14 -0300

    Added configurable return codes

--- ras/ras.py
+++ ras/ras.py
@@ -11,57 +11,64 @@ class RASMock:
         self.config = configparser.ConfigParser()
         self.config.read("ras.ini")
 
-    def pause_user_resources(self, user_id, provider):
-        pass
-
-    def hibernate_user_resources(self, user_id, provider):
-        pass
-
-    def stop_user_resources(self, user_id, provider):
-        pass
-
-    def resume_user_resources(self, user_id, provider):
-        pass
-
-    def purge_user(self, user_id, provider):
-        pass
-    
-    def get_public_key(self):
         key_file_name = self.config["conf"]["public_key_file"]
         file = open(key_file_name)
         key = file.read()
         file.close()
 
-        key = key.replace("\n", "")
-        return jsonpickle.encode({"publicKey": key})
+        self.key = key.replace("\n", "")
+
+        self.pause_return_code = int(self.config["response_code"]["pause"])
+        self.hibernate_return_code = int(self.config["response_code"]["hibernate"])
+        self.stop_return_code = int(self.config["response_code"]["stop"])
+        self.resume_return_code = int(self.config["response_code"]["resume"])
+        self.purge_return_code = int(self.config["response_code"]["purge"])
+
+    def pause_user_resources(self, user_id, provider):
+        return self.pause_return_code
+
+    def hibernate_user_resources(self, user_id, provider):
+        return self.hibernate_return_code
+
+    def stop_user_resources(self, user_id, provider):
+        return self.stop_return_code
+
+    def resume_user_resources(self, user_id, provider):
+        return self.resume_return_code
+
+    def purge_user(self, user_id, provider):
+        return self.purge_return_code
+    
+    def get_public_key(self):
+        return jsonpickle.encode({"publicKey": self.key})
 
 app = Flask(__name__)
 ras_mock = RASMock()
 
 @app.route('/ras/computes/pause/<user_id>/<provider>', methods = ['POST'])
 def pause_user_resources(user_id, provider):
-    ras_mock.pause_user_resources(user_id, provider)
-    return "",200
+    return_code = ras_mock.pause_user_resources(user_id, provider)
+    return "",return_code
 
 @app.route('/ras/computes/hibernate/<user_id>/<provider>', methods = ['POST'])
 def hibernate_user_resources(user_id, provider):
-    ras_mock.hibernate_user_resources(user_id, provider)
-    return "",200
+    return_code = ras_mock.hibernate_user_resources(user_id, provider)
+    return "",return_code
 
 @app.route('/ras/computes/stop/<user_id>/<provider>', methods = ['POST'])
 def stop_user_resources(user_id, provider):
-    ras_mock.stop_user_resources(user_id, provider)
-    return "",200
+    return_code = ras_mock.stop_user_resources(user_id, provider)
+    return "",return_code
 
 @app.route('/ras/computes/resume/<user_id>/<provider>', methods = ['POST'])
 def resume_user_resources(user_id, provider):
-    ras_mock.resume_user_resources(user_id, provider)
-    return "",200
+    return_code = ras_mock.resume_user_resources(user_id, provider)
+    return "",return_code
 
 @app.route('/ras/admin/purge/<user_id>/<provider>', methods = ['DELETE'])
 def purge_user(user_id, provider):
-    ras_mock.purge_user(user_id, provider)
-    return "",200
+    return_code = ras_mock.purge_user(user_id, provider)
+    return "",return_code
 
 @app.route('/ras/publicKey', methods = ['GET'])
 def get_public_key():
